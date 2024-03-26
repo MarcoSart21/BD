c03f75159359ece2603ea3f2f8100f0b2ed43f01
Marcos Arturo Gomez Lopez <marcos.gomez47@unach.mx>
2024-03-26 01:28:26 -0600

    Front 1
    Primera prueba del front

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import *
 
-app=Flask(__name__)
+app=Flask('app')
 
 @app.route('/')
 def  index():
-    return 'Hello World!'
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0' ,port=80)
